0e0aa6c7f4aba90defa538686239d625b47f5a56
aleRizzolo <aless.rizzolo@gmail.com>
2025-04-13 19:58:49 +0200

    fixed security groups definiton

--- aws/modules/vpc/security_group.tf
+++ aws/modules/vpc/security_group.tf
@@ -1,17 +1,17 @@
-# ToDo: make this generic
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and outbound traffic to ecs"
+# ALB Security Group
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  description = "Security group for ALB"
   vpc_id      = aws_vpc.main.id
 
   tags = {
-    Name = "${var.app_name}-allow_tls-sg"
+    Name = "${var.app_name}-alb-sg"
   }
 }
 
-# ALB sg
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+# ALB Ingress Rules
+resource "aws_vpc_security_group_ingress_rule" "alb_allow_tls_ipv4" {
+  security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
@@ -22,13 +22,64 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   }
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_ip4_ecs" {
-  # ToDo: change to allow outbound traffic only to ecs
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+resource "aws_vpc_security_group_ingress_rule" "alb_allow_ipv4" {
+  security_group_id = aws_security_group.alb_sg.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
 
   tags = {
-    Name = "${var.app_name}-allow_outbound_ecs-sg"
+    Name = "${var.app_name}-allow_incoming-sg"
   }
 }
+
+# ECS Security Group for ALB Inbound Traffic
+resource "aws_security_group" "ecs_from_alb_sg" {
+  name        = "ecs-from-alb-sg"
+  description = "Security group for ECS to allow inbound traffic from ALB only"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.app_name}-ecs-from-alb-sg"
+  }
+}
+
+# ECS Security Group for DocumentDB Egress
+resource "aws_security_group" "ecs_to_docdb_sg" {
+  name        = "ecs-to-docdb-sg"
+  description = "Security group for ECS to allow egress to DocumentDB only"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.app_name}-ecs-to-docdb-sg"
+  }
+}
+
+# ALB Egress Rule - restrict to ECS only
+resource "aws_vpc_security_group_egress_rule" "alb_to_ecs" {
+  security_group_id            = aws_security_group.alb_sg.id
+  referenced_security_group_id = aws_security_group.ecs_from_alb_sg.id
+  from_port                    = 0
+  to_port                      = 65535
+  ip_protocol                  = "tcp"
+
+  tags = {
+    Name = "${var.app_name}-alb_to_ecs-sg"
+  }
+}
+
+# ECS Ingress Rule - allow from ALB only
+resource "aws_vpc_security_group_ingress_rule" "ecs_from_alb" {
+  security_group_id            = aws_security_group.ecs_from_alb_sg.id
+  referenced_security_group_id = aws_security_group.alb_sg.id
+  from_port                    = 0
+  to_port                      = 65535
+  ip_protocol                  = "tcp"
+
+  tags = {
+    Name = "${var.app_name}-ecs_from_alb-sg"
+  }
+}
+
+# ToDo: DocumentDB Security Group
