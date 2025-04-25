ef283c1466f14af863329c6ff0fdcd9d38ee43d5
aleRizzolo <aless.rizzolo@gmaail.com>
2025-04-25 12:23:11 +0200

    fix

--- aws/modules/ecs/ecs.tf
+++ aws/modules/ecs/ecs.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 }
 
 resource "aws_ecs_cluster_capacity_providers" "ecs_cluster_capacity" {
-  cluster_name = aws_ecs_cluster.example.name
+  cluster_name = aws_ecs_cluster.ecs_cluster.name
 
   capacity_providers = ["FARGATE"]
 
